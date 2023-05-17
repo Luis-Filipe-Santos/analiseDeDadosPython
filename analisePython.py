07249a40a08d29335630a501ab8215a637612ea6
Luis Filipe Santos Silva <109242677+Luis-Filipe-Santos@users.noreply.github.com>
2023-05-17 18:39:55 -0300

    Inicio do processo

---
+++ analisePython.py
@@ -0,0 +1,18 @@
+# Análise de Dados com Python
+
+### Desafio:
+
+#Você trabalha em uma empresa do varejo e tem milhares de clientes diferentes.
+
+#Com o objetivo de aumentar o faturamento e o lucro da sua empresa, a diretoria quer conseguir identificar quem é o cliente ideal para seus produtos, baseado no histórico de compras dos clientes.
+
+#Para isso, ela fez um trabalho de classificar os clientes com uma nota de 1 a 100. Só que agora, sobrou para você conseguir, a partir dessa nota, descobrir qual o perfil de cliente ideal da empresa.
+
+#Qual a profissão? Qual a idade? Qual a faixa de renda? E todas as informações que você puder analisar para dizer qual o cliente ideal da empresa.
+
+#Base de Dados: https://drive.google.com/drive/folders/1XvNLDKVH7TUS8HdH4r0TkXL__MFpoc3e?usp=share_link
+
+
+import pandas as pd 
+
+
